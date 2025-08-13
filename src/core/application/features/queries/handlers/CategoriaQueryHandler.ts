9b2e68d3e3ed9e02b8f148453cfb388e99c49071
DanteLoay18 <danteloayza8@gmail.com>
2025-08-13 11:58:06 -0500

    se solucion el app response en category get all

--- src/core/application/features/queries/handlers/CategoriaQueryHandler.ts
+++ src/core/application/features/queries/handlers/CategoriaQueryHandler.ts
@@ -1,16 +1,15 @@
 
 // src/core/application/features/queries/handlers/GetCategoriasHandler.ts
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
-import{ CATEGORIA_REPOSITORY, type CategoriaRepository } from 'src/core/domain/ports/outbound';
-import { Categoria } from 'src/core/domain/Categoria';
 import { GetCategoriasQuery } from '../CategoriaQuery';
-import { Inject } from '@nestjs/common';
+import { CategoriaUseCase } from 'src/core/application/services/CategoriaUseCases';
+import { AppResponse } from 'src/infrastructure/http-server/model/app.response';
 
 @QueryHandler(GetCategoriasQuery)
 export class GetCategoriasQueryHandler implements IQueryHandler<GetCategoriasQuery> {
-  constructor( @Inject(CATEGORIA_REPOSITORY)private readonly categoriaRepo: CategoriaRepository) {}
+  constructor( private categoriaUseCase : CategoriaUseCase) {}
 
-  async execute(query: GetCategoriasQuery): Promise<Categoria[]> {
-    return this.categoriaRepo.findAll();
+  async execute(query: GetCategoriasQuery): Promise<AppResponse> {
+    return this.categoriaUseCase.getAllCategoria();
   }
 }
